7cd0e3abd7ec7012d3ad382f809bcf93f614827d
nevtay <nevintay@gmail.com>
2020-01-20 16:08:32 +0800

    First Commit

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./PokemonCard.css"
+import { PokemonType } from "./PokemonType";
+import PokemonBase from "./PokemonBase";
+
+function PokemonCard({ pokemon }) {
+  const { id, name, type, base } = pokemon;
+    return (
+      <div className="pokemon-card">
+        <img className="pokemon-card__img" src={`${process.env.PUBLIC_URL}/pokemonImage/${id}.png`}></img>
+        <span className="pokemon-card__idNumber">#{id}</span>
+        <div className="pokemon-card__name">
+          {
+            <div>
+            <p className="englishName">{name.english}</p>
+            <p className="japaneseName hidden">{name.japanese}</p>
+            <p className="chineseName hidden">{name.chinese}</p>
+            <div>
+              <button className="translateName" onClick={function(e) {
+                    
+                    }
+                  }> Translated Names </button>
+            </div>
+            </div>
+          }
+        </div>
+        <div className="pokemon-card__type_container">
+          {type.map(t => 
+            <PokemonType type={t} /> 
+            )}
+        </div>
+        <div className="pokemon-card__stats">
+          {Object.entries(base).map(stat =>
+            <PokemonBase stats={stat} />
+            )
+          }
+        </div>
+        <script>
+      </script>
+      </div>
+        
+    );
+  }
+
+  export default PokemonCard
