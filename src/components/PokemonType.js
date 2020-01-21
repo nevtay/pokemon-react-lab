089315d2e4bd6ec0e3aeac64395399f9c3045d6b
nevtay <nevintay@gmail.com>
2020-01-21 10:09:06 +0800

    removed commented css

--- src/components/PokemonType.js
+++ src/components/PokemonType.js
@@ -3,18 +3,6 @@ import "./PokemonType.css"
 
 const getTypeClassName = type => {
     return String(type.toLowerCase());
-    // switch (type.toLowerCase()) {
-    //     case("fire"):
-    //         return "fire";
-    //     case("water"):
-    //         return "water";
-    //     case("grass"):
-    //         return "grass";
-    //     case("flying"):
-    //         return "flying";
-    //     default:
-    //         return "unknown";
-    // }
 }
 
 export const PokemonType = ({ type }) => {
